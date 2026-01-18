725732f4cbba0b0ffe1fc120ac39817803fbdcf3
stanikzaiahmadfarid5-jpg <stanikzaiahmadfarid5@gmail.com>
2026-01-18 18:28:56 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 function toggleMenu() {
   const menu = document.getElementById("menu");
-  if (menu.style.left === "0px") {
-    menu.style.left = "-260px";
-  } else {
-    menu.style.left = "0px";
-  }
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
-
-window.onload = () => {
-  document.querySelectorAll(".card").forEach((card, i) => {
-    setTimeout(() => {
-      card.style.opacity = "1";
-      card.style.transform = "translateY(0)";
-    }, i * 120);
-  });
-};
