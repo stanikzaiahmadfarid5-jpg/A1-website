c1ee30ec9d83e9d9e17d739531545a59534542a7
stanikzaiahmadfarid5-jpg <stanikzaiahmadfarid5@gmail.com>
2026-02-11 19:48:28 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.style.display = menu.style.display === "block" ? "none" : "block";
+function toggleMenu(){
+  let menu = document.getElementById("menu");
+  if(menu.style.display === "flex"){
+    menu.style.display = "none";
+  }else{
+    menu.style.display = "flex";
+  }
+}
+
+function checkAnswers(){
+  let correct = document.querySelector('input[data-correct="true"]');
+  let result = document.querySelector('.result');
+
+  if(correct.checked){
+    result.innerText = "✅ Richtig!";
+    result.style.color = "green";
+  }else{
+    result.innerText = "❌ Falsch!";
+    result.style.color = "red";
+  }
 }
